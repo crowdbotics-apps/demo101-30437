cbc3fa492ce64ed6f549d5349af7d561d3595a2b
Crowdbotics <team@crowdbotics.com>
2022-05-17 13:08:04 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class FavoriteCars(models.Model):
+    'Generated Model'
+    make = models.CharField(max_length=256,)
+    model = models.CharField(max_length=256,)
+    year = models.BigIntegerField()
