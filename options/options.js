fdc095375fc0b49f6265bbc2f60ad463c9e508d1
Crowdbotics <team@crowdbotics.com>
2022-05-17 11:53:52 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,6 +1,27 @@
+const styles = StyleSheet.create({
+  hr: {
+    marginTop: 20,
+    marginBottom: 20,
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  },
+  container: { flex: 1, height: 22, padding: 13 },
+  text: { color: "black", fontSize: 20 },
+  buttonPressed: { backgroundColor: "aquamarine" },
+  buttonNotPressed: { backgroundColor: "blue" },
+  button: { borderRadius: 4, padding: 15, marginTop: 10 },
+  buttonText: { color: "white", textAlign: "center", fontSize: 16 }
+})
 export const globalOptions = {
   name: "demo101_30437Identifier",
   url: "https://demo101_30437Identifier.botics.co",
   api: "https://demo101_30437Identifier.botics.co/api/v1"
 }
-export const modulesOptions = {}
+
+export const modulesOptions = {
+  "@modules/app-menu": {
+    title: "App Menu",
+    copy: "Routes available",
+    styles: styles
+  }
+}
